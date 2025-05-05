00eab20c6ec656fa30eca66c0ea1dfb83b721e87
seniru22 <senirubsn@gmail.com>
2025-05-05 12:22:07 +0530

    code changes

--- src/template-builder/template-builder.controller.ts
+++ src/template-builder/template-builder.controller.ts
@@ -1,47 +1,35 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { Template } from 'src/template-builder/template/template.schema';
 import { TemplateService } from './template/template.service';
 import { CreateTemplateDto } from './dto/create-template-dto';
 
-@Controller('template-builder')F
+@Controller('template-builder')
 export class TemplateBuilderController {
-  constructor(private readonly templateService: TemplateService) {}
+    constructor(private readonly templateService: TemplateService) { }
 
     @Post()
     async create(@Body() createTemplateDto: CreateTemplateDto): Promise<{ data: Template; status: number; message: string }> {
         return this.templateService.create(createTemplateDto);
     }
 
-  @Get()
-  async findAll(): Promise<Template[]> {
-    return this.templateService.findAll();
-  }
+    @Get()
+    async find(
+        @Query('id') templateId?: string
+    ): Promise<{ data: Template | { [key: string]: Template }; status: number; message: string }> {
+        if (templateId) {
+            const template = await this.templateService.findOne(templateId);
+            return template;
+        }
 
-  @Get()
-  async find(
-    @Query('name') name?: string,
-  ): Promise<Template | Template[] | null> {
-    if (name) {
-      return this.templateService.findOne(name);
+        const allTemplates = await this.templateService.findAll();
+        return allTemplates;
     }
-    return this.templateService.findAll();
-  }
 
-  @Patch()
-  async updateByQuery(
-    @Query('id') id: string,
-    @Body() updateDto: Partial<Template>,
-  ): Promise<Template> {
-    return this.templateService.update(id, updateDto);
-  }
+
+    @Patch()
+    async updateByQuery(@Query('id') id: string, @Body() createTemplateDto: CreateTemplateDto): Promise<Template> {
+        return this.templateService.update(id, createTemplateDto);
+    }
 
     @Delete()
     async deleteByQuery(@Query('id') id: string): Promise<{ message: string }> {
